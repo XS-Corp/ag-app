8485418d1f4e0a35e566d42dda21d47520694fe0
XS-Corp <141255555+XS-Corp@users.noreply.github.com>
2025-10-16 22:50:00 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, BrowserView, ipcMain, shell, dialog } = require('ele
 const path = require('path');
 const fs = require('fs');
 
-const HOMEPAGE = 'https://google.com/';
+const HOMEPAGE = 'https://search.kickedstorm.com/';
 const EXT_DIR = path.join(__dirname, 'extensions');
 
 let win;
