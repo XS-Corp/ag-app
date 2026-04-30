686a6210deb7e164593f4d03c972a4a114d62ba4
XS-Corp <141255555+XS-Corp@users.noreply.github.com>
2026-04-30 04:27:07 -0700

    Add license and copyright notice to preload.js
    Added license information and copyright notice to preload.js

--- preload.js
+++ preload.js
@@ -1,3 +1,9 @@
+/**
+ * (c) 2026 KickedStorm (kickedstorm.com)
+ * Project: AG Browser
+ * License: GNU AGPLv3
+ * Unauthorized copying of this file is strictly prohibited.
+ */
 const { contextBridge, ipcRenderer } = require('electron');
 
 window.addEventListener('DOMContentLoaded', () => {
